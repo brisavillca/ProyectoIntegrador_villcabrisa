a70c09a3a3c1075a075e2138ccd889dd2cc1a945
brisavillca <brisavillca@gmail.com>
2022-05-25 20:47:53 -0300

    header

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -8,6 +8,11 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercadeComponent } from './components/acercade/acercade.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { CursosComponent } from './components/cursos/cursos.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +21,16 @@ import { AcercadeComponent } from './components/acercade/acercade.component';
     LogoAPComponent,
     SocialComponent,
     BannerComponent,
-    AcercadeComponent
+    AcercadeComponent,
+    ExperienciaComponent,
+    CursosComponent,
+    EducacionComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
