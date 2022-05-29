289d243af35642b3eb958c26a5fd431d064ef137
brisavillca <brisavillca@gmail.com>
2022-05-29 02:06:09 -0300

    Creacion de BackEnd

---
+++ ProyectoIntegrador/BackEnd/src/test/java/com/portfolio/vb/VbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.vb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ProyectoIntegrador/BackEnd/vb/src/main/java/com/portfolio/vb/VbApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.vb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VbApplication.class, args);
+	}
+
+}
